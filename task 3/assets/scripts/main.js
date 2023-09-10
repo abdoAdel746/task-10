0f9f45e4e12af3656405151ca82f8e61b1227fe6
abdelrahman adel <abdelrahman20191700364@cis.asu.edu.eg>
2023-09-10 16:55:34 +0300

    up3

--- task 3/assets/scripts/main.js
+++ task 3/assets/scripts/main.js
@@ -1,6 +1,8 @@
 "use strict";
 let img_class = document.querySelectorAll("#img_contaitner img");
 let main_img = document.querySelector("section");
+let h1 = document.querySelector("h1")
+let sec_content = ["change background 1","change background 2","change background 3",]
 
 img_class.forEach((e) => {
   e.addEventListener("click", () => {
@@ -13,9 +15,13 @@ img_class.forEach((e) => {
     font-size: 50px;
     font-family: sans-serif;
     letter-spacing: 3px;
-    background: linear-gradient(0deg, rgb(0 0 0 / 52%), rgb(0 0 0 / 67%)), url("${e.getAttribute('src')}");
+    background: linear-gradient(0deg, rgb(0 0 0 / 52%), rgb(0 0 0 / 67%)), url("${e.getAttribute(
+      "src"
+    )}");
     background-size: cover;
-    position: relative; }`
+    position: relative;
+    justify-content: right; }`;
+    let img_num = e.getAttribute("src").match(/\d+/g);
+    h1.innerHTML = sec_content[img_num-1]
   });
- 
 });
